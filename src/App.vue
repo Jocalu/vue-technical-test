8f1c34b339dc4d1cf27657140a2c112346be4a26
jocalu <jocalu87@gmail.com>
2023-01-26 11:01:26 +0100

    feat: add default layout and use it in app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 
-import Datepicker from 'vue3-datepicker'
-
-import CustomSelect from '@/components/CustomSelect/CustomSelect.vue'
 import BreadcrumbsRooms from '@/components/BreadcrumbsRooms/BreadcrumbsRooms.vue'
-import FooterDefault from '@/components/FooterDefault/FooterDefault.vue'
-import HeaderDefault from '@/components/HeaderDefault/HeaderDefault.vue'
+import HeroMenu from '@/components/HeroMenu/HeroMenu.vue'
+import LayoutDefault from '@/layouts/default.vue'
 import ReservationSummary from '@/components/ReservationSummary/ReservationSummary.vue'
 import RoomsContainer from '@/components/RoomsContainer/RoomsContainer.vue'
 
@@ -14,19 +11,13 @@ import { Room } from '@/@types/Room'
 
 export default defineComponent({
   components: {
-    CustomSelect,
     BreadcrumbsRooms,
-    Datepicker,
-    FooterDefault,
-    HeaderDefault,
+    HeroMenu,
+    LayoutDefault,
     RoomsContainer,
     ReservationSummary
   },
   setup() {
-    const sd = ref()
-    const ed = ref()
-    const adults = ref('')
-    const children = ref('')
     const summary = ref<typeof ReservationSummary>()
 
     const selectRoom = (room: Room) => {
@@ -35,77 +26,25 @@ export default defineComponent({
       }
     }
 
-    const modifyReservationHandler = () => {
-      if (summary.value) {
-        summary.value.updateReservationData({
-          startDate: new Intl.DateTimeFormat('en-US').format(sd.value),
-          endDate: new Intl.DateTimeFormat('en-US').format(ed.value),
-          adults: adults.value
-        })
-        alert('Summary Updated')
-      }
-    }
     return {
-      sd,
-      ed,
-      adults,
-      children,
       selectRoom,
-      summary,
-      modifyReservationHandler
+      summary
     }
   }
 })
 </script>
 
 <template>
-  <HeaderDefault />
-  <div class="mb-4 hero">
-    <div class="flex justify-center px-8 py-4 bg-primary bg-opacity-40">
-      <div class="flex items-start space-x-4">
-        <div class="calendar-wrapper">
-          <Datepicker v-model="sd" class="calendar-input" />
-        </div>
-        <div class="calendar-wrapper">
-          <Datepicker v-model="ed" class="calendar-input" />
-        </div>
-        <CustomSelect>
-          <select v-model="adults">
-            <option value="">Select Adults</option>
-            <option value="1">Adults: 1</option>
-            <option value="2">Adults: 2</option>
-            <option value="3">Adults: 3</option>
-            <option value="4">Adults: 4</option>
-            <option value="5">Adults: 5</option>
-          </select>
-        </CustomSelect>
-        <CustomSelect>
-          <select v-model="children">
-            <option value="">Select Children</option>
-            <option value="0">Children: 0</option>
-            <option value="1">Children: 1</option>
-            <option value="2">Children: 2</option>
-            <option value="3">Children: 3</option>
-            <option value="4">Children: 4</option>
-            <option value="5">Children: 5</option>
-            <option value="6">Children: 6</option>
-          </select>
-        </CustomSelect>
-        <button class="primary-btn" @click="modifyReservationHandler">
-          Modify
-        </button>
+  <LayoutDefault>
+    <HeroMenu class="mb-4" />
+    <BreadcrumbsRooms />
+    <div class="inline-block px-8 mb-16">
+      <div class="float-left w-2/3">
+        <RoomsContainer :select-room="selectRoom" />
+      </div>
+      <div class="float-right w-1/3 pl-4">
+        <ReservationSummary ref="summary" />
       </div>
     </div>
-  </div>
-  <BreadcrumbsRooms />
-  <div class="inline-block px-8 mb-16">
-    <div class="float-left w-2/3">
-      <RoomsContainer :select-room="selectRoom" />
-    </div>
-    <div class="float-right w-1/3 pl-4">
-      <ReservationSummary ref="summary" />
-    </div>
-  </div>
-  <FooterDefault />
+  </LayoutDefault>
 </template>
-<style scoped src="./App.css"></style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import HeaderDefault from '@/components/HeaderDefault/HeaderDefault.vue'
+import FooterDefault from '@/components/FooterDefault/FooterDefault.vue'
+</script>
+
+<template>
+  <HeaderDefault />
+  <slot />
+  <FooterDefault />
+</template>
